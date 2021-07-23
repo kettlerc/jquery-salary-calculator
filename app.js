296ef1106437b9a1b27a67a5a269d4e055062015
Curtis Kettler <curtiskettler@gmail.com>
2021-07-23 07:51:02 -0500

    addEmployee function apppending to table correctly

--- app.js
+++ app.js
@@ -5,4 +5,21 @@ $(document).ready(onReady);
 function onReady() {
     console.log('JQ');
     
+    $(document).on('click', '#submit', addEmployee);
+}
+
+function addEmployee() {
+    console.log('button works');
+    let firstName = $('#firstNameIn').val();
+    let lastName = $('#lastNameIn').val();
+    let idNum = $('#idNumIn').val();
+    let title = $('#titleIn').val();
+    let annualSalary = $('#annSalIn').val();
+    let markup = `<tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${idNum}</td>
+        <td>${title}</td>
+        <td>${annualSalary}</td>`;
+    $('table tbody').append(markup);
 }
