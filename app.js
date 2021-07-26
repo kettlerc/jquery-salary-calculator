f5747ea26a65e597eae11529db71ba5213c301b5
Curtis Kettler <curtiskettler@gmail.com>
2021-07-26 15:11:29 -0500

    updated after live solve

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(onReady);
 function onReady() {
     console.log('JQ');
     $(document).on('submit', '#inputForm', addEmployee);
-    $(document).on('click', '#delete', deleteRow);
+    $(document).on('click', '.delete', deleteRow);
 } //end onReady
 
 function addEmployee(event) {
@@ -22,35 +22,39 @@ function addEmployee(event) {
     employees.push(employee);
     console.log(employee);
 
+    calculateMonthlyCost();
     renderToDom();
+} //end addEmployee
 
+function calculateMonthlyCost() {
     let total = 0;
     for (let employee of employees) {
-        total += employee.annualSalary / 12;
-        
+        total += employee.annualSalary / 12;  
     };
-    $('#totalMonthlyCost').text(total);
+    $('#totalMonthlyCost').text(total.toFixed(2));
     if (total > 20000) {
         console.log('too much');
         $('#totalCost').addClass('inTheRed');
     }
-} //end addEmployee
+} //end calculateMonthlyCost
+
+function deleteRow() {
+    employees.splice($(this).data( 'index' ), 1 );
+    $(this).parent().parent().remove();
+    calculateMonthlyCost();
+} //end deleteRow
 
 function renderToDom() {
     $('table tbody').empty();
-    for (let employee of employees) {
+    for (let i=0; i<employees.length; i++) {
+        const thisEmployee = employees[ i ];
     $('table tbody').append(`<tr>
-        <td>${employee.firstName}</td>
-        <td>${employee.lastName}</td>
-        <td>${employee.idNum}</td>
-        <td>${employee.title}</td>
-        <td>$${employee.annualSalary}</td>
-        <td><button id="delete">Delete</button></td>`);
+        <td>${thisEmployee.firstName}</td>
+        <td>${thisEmployee.lastName}</td>
+        <td>${thisEmployee.idNum}</td>
+        <td>${thisEmployee.title}</td>
+        <td>$${thisEmployee.annualSalary}</td>
+        <td><button class="delete" data-index="{i}">Delete</button></td>`);
     };
     $('input').val('');
 } //end renderToDom
-
-function deleteRow() {
-    $(this).parent().parent().remove();
-} //end deleteRow
-
